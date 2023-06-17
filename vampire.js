f16b6d217ef738dfed2198de328994371b5055f8
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-17 14:30:01 -0400

    vampire project

--- vampire.js
+++ vampire.js
@@ -1,3 +1,4 @@
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
@@ -10,35 +11,73 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
+  isMoreSeniorThan(Vampire) {
+    return this.numberOfVampiresFromOriginal < Vampire.numberOfVampiresFromOriginal;
   }
 
-  /** Stretch **/
-
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    }
+    let senior;
+    let junior;
+    if (this.isMoreSeniorThan(vampire)) {
+      senior = this;
+      junior = vampire;
+    } else {
+      junior = this;
+      senior = vampire;
+    }
+
+    if (senior.isDirectAncestor(junior)) {
+      return senior;
+    } else {
+      return senior.creator.closestCommonAncestor(junior);
+    }
 
   }
+
+  // is a direct ancestor
+  // if they are one of my offspring
+  //  ...or, they are a direct ancestor of one of my offspring
+
+  isDirectAncestor(vampire) {
+    if (this.offspring.includes(vampire)) {
+      return true;
+    } else {
+      for (let v of this.offspring) {
+        if (v.isDirectAncestor(vampire)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
 }
+
 
 module.exports = Vampire;
 
